51ffd6ff6638278dce186e82a1408f3f70ab7430
arinrahman <rahman.arin29@gmail.com>
2025-09-07 20:49:51 -0600

    Add setup-ldms.sh bootstrap script

---
+++ setup-ldms.sh
@@ -0,0 +1,13 @@
+﻿set -e
+export DEBIAN_FRONTEND=noninteractive
+apt-get update -y
+apt-get install -y git build-essential autoconf automake libtool pkg-config gettext
+cd /root
+test -d ovis || git clone https://github.com/ovis-hpc/ovis.git
+cd ovis
+./autogen.sh
+./configure --prefix=/opt/ovis
+make -j"$(nproc)"
+make install
+echo 'export PATH=/opt/ovis/bin:$PATH' > /etc/profile.d/ldms.sh
+echo "[OK] LDMS installed to /opt/ovis"
